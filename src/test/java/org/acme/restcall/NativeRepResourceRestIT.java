5a69095669547d7f9eda92a86eaff0e3b43bde05
deepakbajaj <deepakbajaj.tu@gmail.com>
2021-12-29 11:58:33 +0000

    Initial code check in

---
+++ src/test/java/org/acme/restcall/NativeRepResourceRestIT.java
@@ -0,0 +1,9 @@
+package org.acme.restcall;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeRepResourceRestIT extends RepResourceRestTest {
+
+    // Execute the same tests but in native mode.
+}
